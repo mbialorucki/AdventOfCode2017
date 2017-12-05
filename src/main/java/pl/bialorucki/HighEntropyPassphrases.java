a56d4a28ccc060935dbee73ebdaba4afd906b7d9
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2017-12-05 07:51:16 +0100

    Day 4 - High-Entropy Passphrases - part two

--- src/main/java/pl/bialorucki/HighEntropyPassphrases.java
+++ src/main/java/pl/bialorucki/HighEntropyPassphrases.java
@@ -16,4 +16,28 @@ public class HighEntropyPassphrases {
         return !collectedElements.values().stream().anyMatch(x -> x > 1);
 
     }
+
+    public static boolean checkUsingAnagramDetection(String passphrase) {
+        String[] splittedPassphrase = passphrase.split(" ");
+        for (int i = 0; i < splittedPassphrase.length - 1; i++) {
+            for(int j = i + 1 ; j < splittedPassphrase.length ; j++) {
+                if (isAnagram(splittedPassphrase[i], splittedPassphrase[j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    public static boolean isAnagram(String firstString, String secondString) {
+        if (firstString.length() != secondString.length())
+            return false;
+        char[] firstArray = firstString.toCharArray();
+        char[] secondArray = secondString.toCharArray();
+
+        Arrays.sort(firstArray);
+        Arrays.sort(secondArray);
+        return Arrays.equals(firstArray, secondArray);
+    }
 }
--- src/test/java/pl/bialorucki/HighEntropyPassphrasesTest.java
+++ src/test/java/pl/bialorucki/HighEntropyPassphrasesTest.java
@@ -1,6 +1,8 @@
 package pl.bialorucki;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,5 +23,37 @@ class HighEntropyPassphrasesTest {
     public void should_return_true_for_sililar_words(){
         String testPassphrase = "aa bb cc dd aaa";
         assertTrue(HighEntropyPassphrases.check(testPassphrase));
+    }
+
+    @Test
+    public void should_detect_anagram(){
+        String firstString = "abcde";
+        String secondString = "ecdab";
+        assertTrue(HighEntropyPassphrases.isAnagram(firstString,secondString));
+    }
+
+    @Test
+    public void should_detect_not_anagram(){
+        String firstString = "abc";
+        String secondString = "zhh";
+        assertFalse(HighEntropyPassphrases.isAnagram(firstString,secondString));
+    }
+    @Test
+    public void should_detect_not_anagram_for_diffren_string_lengths(){
+        String firstString = "abc";
+        String secondString = "z";
+        assertFalse(HighEntropyPassphrases.isAnagram(firstString,secondString));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcde fghij","a ab abc abd abf abj","iiii oiii ooii oooi oooo"})
+    public void should_return_true_for_non_anagram_items(String passPhrase){
+        assertTrue(HighEntropyPassphrases.checkUsingAnagramDetection(passPhrase));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcde xyz ecdab","oiii ioii iioi iiio"})
+    public void should_return_false_for_anagram_items(String passPhrase){
+        assertFalse(HighEntropyPassphrases.checkUsingAnagramDetection(passPhrase));
     }
 }
